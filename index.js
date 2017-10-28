af7c2bd632c734ec52473d94bf330d6b3496eba6
jeremyfourna <jeremyfourna@gmail.com>
2017-10-28 09:49:42 +0200

    Construct boxscore

--- index.js
+++ index.js
@@ -1,185 +1,41 @@
 const R = require('ramda');
+const { generateGame, initGame, initPlayers } = require('./src/game-generation');
 
-function change(zeroOrOne) {
-  return R.ifElse(
-    R.equals(0),
-    R.always(1),
-    R.always(0)
-  )(zeroOrOne);
-}
+const gameConfig = ['fiba', ['normal', 'normal']];
 
-function minutesInAGame(style, teamsSpeed) {
-  function secondsInAPossession(remainingSec, nbPlays = 0, whoIsPlaying = 0) {
-    const secToRemove = R.cond([
-      [R.equals('fast'), R.always(12)],
-      [R.equals('slow'), R.always(20)],
-      [R.equals('normal'), R.always(15)],
-      [R.T, R.always(24)]
-    ]);
+const game = generateGame(initGame(...gameConfig, initPlayers()));
 
-    if (R.lte(remainingSec, 0)) {
-      return nbPlays;
+function boxscore(game) {
+  function buildBoxscore(history, team1, team2) {
+    function index(shoot, team) {
+      return R.findIndex(R.propEq('id', R.head(shoot)), team);
+    }
+
+    function newTeam(shoot, team) {
+      return R.adjust(player => R.assoc('pts', R.add(R.last(shoot), R.prop('pts', player) ||  0), player), index(shoot, team), team);
+    }
+
+    if (R.equals(R.length(history), 0)) {
+      return [team1, team2];
     } else {
-      return secondsInAPossession(
-        R.subtract(remainingSec, secToRemove(R.nth(whoIsPlaying, teamsSpeed))),
-        R.inc(nbPlays),
-        change(whoIsPlaying)
-      );
+      const shoot = R.head(history);
+      const shootIsInTeam1 = R.contains(R.head(shoot), R.map(cur => R.prop('id', cur), team1));
+
+      if (R.equals(shootIsInTeam1, true)) {
+
+        return buildBoxscore(R.tail(history), newTeam(shoot, team1), team2);
+      } else {
+
+        return buildBoxscore(R.tail(history), team1, newTeam(shoot, team2));
+      }
     }
   }
 
-  const secForStyle = R.cond([
-    [R.equals('nba'), R.always(R.multiply(48, 60))],
-    [R.equals('fiba'), R.always(R.multiply(40, 60))],
-    [R.equals('overtime'), R.always(R.multiply(5, 60))]
-  ]);
+  const playersWhoScored = R.prop('history', game);
+  const teams = R.prop('players', game);
 
-  return secondsInAPossession(secForStyle(style));
+  return buildBoxscore(playersWhoScored, ...teams);
 }
 
-function initGame(style, teamsSpeed, players) {
-
-  const defaultConfig = R.assoc('remainingPossessions', R.__, {
-    style,
-    teamsSpeed,
-    players,
-    history: [],
-    score: [0, 0],
-    teamWithBall: 0,
-    possessionsPlayed: 0,
-  });
-
-  return R.cond([
-    [R.equals('nba'), style => defaultConfig(minutesInAGame(style, teamsSpeed))],
-    [R.equals('fiba'), style => defaultConfig(minutesInAGame(style, teamsSpeed))],
-    [R.T, style => `We only know nba or fiba as basket-ball style, ${style} is unknown`]
-  ])(style);
-}
-
-function willScore(shoot, playerCharact) {
-  function probabilityToScore(percentage) {
-    return (R.gte(Math.random(), R.add(percentage, scaleForScoreChance(playerCharact)))) ? true : false;
-  }
-
-  function scaleForScoreChance(playerCharact) {
-    return R.cond([
-      [R.flip(R.gte)(90), R.always(-0.15)],
-      [R.flip(R.gte)(80), R.always(-0.10)],
-      [R.flip(R.gte)(70), R.always(-0.05)],
-      [R.flip(R.gte)(60), R.always(0)],
-      [R.flip(R.gte)(50), R.always(0.1)],
-      [R.flip(R.gte)(40), R.always(0.2)],
-      [R.flip(R.gte)(30), R.always(0.3)],
-      [R.flip(R.gte)(20), R.always(0.4)],
-      [R.flip(R.gte)(10), R.always(0.5)],
-      [R.T, R.always(0.7)]
-    ])(playerCharact);
-  }
-  // On average players will have an accuracy between 80%
-  // and 90% at the Free Thrown exercice
-  // so 1 - 0.8 = 0.2
-  // On average players will have an accuracy between 40%
-  // and 50% at the 2pts exercice
-  // so 1 - 0.4 = 0.6
-  // On average players will have an accuracy between 30%
-  // and 40% at the 3pts exercice
-  // so 1 - 0.3 = 0.7
-  return R.cond([
-    [R.equals(1), () => probabilityToScore(0.2)],
-    [R.equals(2), () => probabilityToScore(0.6)],
-    [R.equals(3), () => probabilityToScore(0.7)]
-  ])(shoot);
-}
-
-function typeOfShoot() {
-  // On average their are 110 shoots per game
-  // 20 are FT -> 20 / 110 = 18,18%
-  // 30 are 3pts -> 30 / 110 = 27,27%
-  // 60 are 2pts -> 60 / 110 = 54,54%
-  const probaFT = R.divide(20, 110);
-  const proba3pts = R.divide(30, 110);
-  const proba2pts = R.divide(60, 110);
-  return R.cond([
-    [R.flip(R.gte)(proba2pts), R.always(2)],
-    [R.flip(R.gte)(proba3pts), R.always(3)],
-    [R.flip(R.lte)(probaFT), R.always(1)]
-  ])(Math.random());
-}
-
-function whoWillShoot(team) {
-  const player = Math.floor(Math.random() * 10);
-  return R.nth(player, team);
-}
-
-function charactForShoot(shoot) {
-  return R.cond([
-    [R.equals(1), R.always('ft')],
-    [R.equals(2), R.always('twoPts')],
-    [R.equals(3), R.always('threePts')]
-  ])(shoot);
-}
-
-function generateGame(config) {
-  //console.log(config);
-  const shoot = typeOfShoot();
-  const player = whoWillShoot(R.nth(R.prop('teamWithBall', config), R.prop('players', config)));
-  const shootResult = willScore(shoot, R.path(['charact', charactForShoot(shoot)], player));
-
-  if (R.equals(R.prop('remainingPossessions', config), 0)) {
-    if (R.equals(R.head(R.prop('score', config)), R.last(R.prop('score', config)))) {
-      const newConfig = R.assoc('remainingPossessions', minutesInAGame('overtime', R.prop('teamsSpeed', config)), config);
-      const newConfig1 = R.assoc('overtime', true, newConfig);
-      return generateGame(newConfig1);
-    } else {
-
-      return config;
-    }
-  } else if (R.equals(shootResult, true)) {
-    const transformations = {
-      remainingPossessions: R.dec,
-      score: R.adjust(R.add(shoot), R.prop('teamWithBall', config)),
-      teamWithBall: change,
-      possessionsPlayed: R.inc,
-      history: R.append([R.prop('id', player), shoot])
-    };
-    const newConfig = R.evolve(transformations, config);
-
-    return generateGame(newConfig);
-  } else {
-    const transformations = {
-      remainingPossessions: R.dec,
-      teamWithBall: change,
-      possessionsPlayed: R.inc
-    };
-    const newConfig = R.evolve(transformations, config);
-
-    return generateGame(newConfig);
-  }
-}
-
-function initPlayers() {
-  function generateId() {
-    return Math.round(Math.random() * 1000000000);
-  }
-
-  function generateTeam(num) {
-    return R.map(cur => {
-      return {
-        id: generateId(),
-        name: 'Mickael Jordan',
-        charact: {
-          ft: Math.floor(Math.random() * (100 - 70) + 70),
-          twoPts: Math.floor(Math.random() * (100 - 50) + 50),
-          threePts: Math.floor(Math.random() * (100 - 40) + 40)
-        }
-      };
-    }, R.range(0, num));
-  }
-
-  return [generateTeam(10), generateTeam(10)];
-}
-
-console.log('generateGame()', generateGame(initGame('fiba', ['normal', 'normal'], initPlayers())));
-
-exports.generateGame = generateGame;
-exports.initGame = initGame;
+console.log(R.prop('score', game));
+console.log(boxscore(game));
